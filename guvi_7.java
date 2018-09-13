34b5a4bea9f7813e68ed49f131ac09b4e6a12f07
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 16:06:35 +0530

    hello n times

---
+++ guvi_7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Hello{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int i=1;
+
+    while(i<=n)
+    {
+      System.out.println("Hello!");
+      i++;
+    }
+  }
+}
