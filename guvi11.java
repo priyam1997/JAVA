7347f028b2bd71ff6c2ceea042f9da197d33732f
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 18:15:56 +0530

    n to the power m

---
+++ guvi11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Pnm{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int m = scan.nextInt();
+    int result = (int)Math.pow(n,m);
+    System.out.println(result);
+  }
+}
