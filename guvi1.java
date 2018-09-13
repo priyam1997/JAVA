79bf1fe60bd433bcde52e366664536d476e9ad8a
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 15:01:11 +0530

    check negetive positive number

---
+++ guvi1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class PNnum{
+  public static void main(String[] args) {
+    Scanner scan  =  new Scanner(System.in);
+    int n = scan.nextInt();
+
+    if(n<0)
+    {
+      System.out.println("Negetive");
+    }
+    else
+    {
+      System.out.println("Positive");
+    }
+  }
+}
