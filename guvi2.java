6ef7d1ab0d3455b14ff66880e9548fafef84f9b5
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 15:07:05 +0530

    check even odd number

---
+++ guvi2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class EOnum{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+
+    if(n%2==0)
+    {
+      System.out.println("Even number");
+    }
+    else
+    {
+      System.out.println("Odd number");
+    }
+  }
+}
