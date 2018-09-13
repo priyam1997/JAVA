d62b3c7096c4b1bbaeb8ac4137cb4725820e4291
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 16:59:56 +0530

    number of digits in a n digit number

---
+++ guvi10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class N{
+  public static void main(String[] args) {
+   Scanner scan = new Scanner(System.in);
+   String n = scan.next();
+
+   String[] num = n.split("");
+   System.out.println(num.length);
+  }
+}
