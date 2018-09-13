966ebc07f51a0d19ba740f5b36ad69f1fe4799d3
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-13 16:36:44 +0530

    Hello

---
+++ guvi_7.java
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+import java.util.*;
+class Hello{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int i=1;
+
+    while(i<=n)
+    {
+      System.out.println("Hello!");
+      i++;
+    }
+  }
+}
+=======
+import java.util.*;
+class Hello{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int i=1;
+
+    while(i<=n)
+    {
+      System.out.println("Hello!");
+      i++;
+    }
+  }
+}
+>>>>>>> origin
