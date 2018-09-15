30b63bf01c040b09e03ba78e21f8db1114408117
priyam1997 <30360706+priyam1997@users.noreply.github.com>
2018-09-15 11:40:17 +0530

    Even number b/w range

---
+++ guvi15.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Even {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        for(int i=n;i<m;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i+"");
+            }
+        }
+
+    }
+}
